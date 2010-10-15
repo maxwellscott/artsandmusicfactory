1c9bcb16fbb541558253357e70c001b125a6f2fa
Max <msprior@gmail.com>
2010-10-14 23:05:08 -0400

    User model update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101014234042) do
+ActiveRecord::Schema.define(:version => 20101015025515) do
 
   create_table "fans", :force => true do |t|
     t.string   "email"
